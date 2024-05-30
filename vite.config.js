feed33cdd2be3a52361b518a8d89514641900b32
Lamya Gandhi <lamya.gandhi4@gmail.com>
2024-05-30 10:27:16 +0400

    Deployed website4

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,23 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build:[ {
-    outDir: 'dist'  // Ensure this is set to 'dist'
-  }],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const basic = id.toString().split('node_modules/')[1];
+            const sub1 = basic.split('/')[0];
+            if (sub1 !== '.pnpm') {
+              return sub1.toString();
+            }
+            const name2 = basic.split('/')[1];
+            return name2.split('@')[name2[0] === '@' ? 1 : 0].toString();
+          }
+        }
+      }
+    }
+  },
   plugins: [react()],
-assetsInclude:['**/*.glb']
-})
+  assetsInclude: ['**/*.glb']
+});
