bf3ee415ffedb5ebfbc8d6159a45648dab61eb53
Lamya Gandhi <lamya.gandhi4@gmail.com>
2024-05-30 10:06:31 +0400

    Deployed website3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build:[ {
+    outDir: 'dist'  // Ensure this is set to 'dist'
+  }],
   plugins: [react()],
 assetsInclude:['**/*.glb']
 })
